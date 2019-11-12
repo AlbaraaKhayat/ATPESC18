0d472a74b284c0a49f07c36489255f37800eb454
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-11-12 22:59:57 +0300

    Update simple.cu

--- examples/cuda/simple/simple.cu
+++ examples/cuda/simple/simple.cu
@@ -3,31 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void simpleKernel(int N, float *d_a){
+__global__ void simpleKernel(int N, float *d_a, float *d_b){
 	   
   // Convert thread and thread-block indices into array index 
   const int n  = threadIdx.x + blockDim.x*blockIdx.x;
 	   
   // If index is in [0,N-1] add entries
   if(n<N)
-    d_a[n] = n;
+    d_a[N-1-n] = d_b[n];
 }
 
 int main(int argc,char **argv){
   int N = 512; // size of array for this DEMO
-    
-  float *d_a;  // Allocate DEVICE array
+     // HOST array
+  float *h_a = (float*) calloc(N, sizeof(float));
+  float *h_b = (float*) calloc(N, sizeof(float)); 
+     // Allocate DEVICE array
+  float *d_a, *d_b; 
   cudaMalloc((void**) &d_a, N*sizeof(float));
-    
+  cudaMalloc((void**) &d_b, N*sizeof(float));
   dim3 dimBlock(512,1,1);          // 512 threads per thread-block
   dim3 dimGrid((N+511)/512, 1, 1); // Enough thread-blocks to cover N
-    
+  
+    // Init. HOST array    
+  for(int n=0;n<N;++n){
+      h_b[n] = n;
+  }
+  cudaMemcpy(d_b, h_b, N*sizeof(float), cudaMemcpyDeviceToHost);    
   // Queue kernel on DEVICE
-  simpleKernel <<< dimGrid, dimBlock >>> (N, d_a);
+  simpleKernel <<< dimGrid, dimBlock >>> (N, d_a, d_b);
     
-  // HOST array
-  float *h_a = (float*) calloc(N, sizeof(float));
-    
+
+
   // Transfer result from DEVICE to HOST
   cudaMemcpy(h_a, d_a, N*sizeof(float), cudaMemcpyDeviceToHost);
     
